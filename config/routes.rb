3719e6ea4b8d3c717245fe9554a02fcd5491dbd7
Adil Tuladhar <adiltuladhar77@gmail.com>
2022-07-06 22:36:41 +0545

    Override the Rails Resources Routes for the Show Action

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  # get 'pages/home'
+  root "pages#home"
+  # get 'pages/about'
+  get 'about', to: 'pages#about'
+  # get 'pages/contact'
+  get 'contact', to: 'pages#contact'
   resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
