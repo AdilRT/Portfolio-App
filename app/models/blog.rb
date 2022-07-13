870c992f1ba77e8dc7dce26b6eeb9b37811c0dee
Adil Tuladhar <adiltuladhar77@gmail.com>
2022-07-13 22:59:56 +0545

    Updated status for enum

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,8 @@
 class Blog < ApplicationRecord
+  enum status: {draft: 0, published: 1}
+
   extend FriendlyId
   # generate a slug using the title of the blog
   friendly_id :title, use: :slugged
 end
+ 
