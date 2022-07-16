8a68c796aaad3cc9bd6f9cb89e675aa2b812c46a
Adil Tuladhar <adiltuladhar77@gmail.com>
2022-07-16 20:03:14 +0545

    Implemented validations for blogs,skills and portfolio items

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,5 +4,7 @@ class Blog < ApplicationRecord
   extend FriendlyId
   # generate a slug using the title of the blog
   friendly_id :title, use: :slugged
+
+  validates_presence_of :title, :body
 end
  
