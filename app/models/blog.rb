21c4c6509ca9ed507455995cabd46c660c7bf4de
Adil Tuladhar <adiltuladhar77@gmail.com>
2022-07-18 23:40:38 +0545

    Integrated association between Blogs and Topic

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,10 +1,9 @@
 class Blog < ApplicationRecord
   enum status: {draft: 0, published: 1}
-
   extend FriendlyId
   # generate a slug using the title of the blog
   friendly_id :title, use: :slugged
-
   validates_presence_of :title, :body
+
 end
  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_06_180543) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_18_175056) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_06_180543) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
