ebd119e2c939a7c704d9243bdf33cc03cef1ef68
Adil Tuladhar <adiltuladhar77@gmail.com>
2022-09-01 23:02:13 +0545

    Implemented default values for Porfolios

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   # get 'pages/home'
   root "pages#home"
+
   # get 'pages/about'
   get 'about', to: 'pages#about'
+
   # get 'pages/contact'
   get 'contact', to: 'pages#contact'
+
   resources :blogs do
     member do
       get :toggle_status
