7adb4ef36c7261ce0db17336c761065aa26639e3
Adil Tuladhar <adiltuladhar77@gmail.com>
2022-09-06 00:22:45 +0545

    Implemented form configuration for Nested attributes in model and view

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,10 +1,11 @@
 class Portfolio < ApplicationRecord
   include Placeholder
 
+  after_initialize :set_defaults
   scope :ror_portfolio_items, -> {where(subtitle: 'RoR')}
   has_many :technologies
   validates_presence_of :title, :body, :main_image, :thumb_image
-  after_initialize :set_defaults
+  accepts_nested_attributes_for :technologies, reject_if: lambda {|attrs| attrs['name'].blank? }
 
   def self.angular
   where(subtitle: 'Angular')
