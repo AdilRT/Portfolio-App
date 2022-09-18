d82445ec4a2433305e24dfba351fe5d930250c33
Adil Tuladhar <adiltuladhar77@gmail.com>
2022-09-18 22:37:04 +0545

    Added custom attrubutes to Devise user registration and editin account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # bad practice to permit in custom attributes of devise in application_controller
+  # run this before you run anything else in the controller
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
 end
