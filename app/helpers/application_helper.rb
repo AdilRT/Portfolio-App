56505b6882bde117f81ed89c1078aa7ec8450702
Adil Tuladhar <adiltuladhar77@gmail.com>
2022-10-19 00:45:06 +0545

    Implemented source_tracker_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,13 @@ module ApplicationHelper
     else
       (link_to 'Register', new_user_registration_path) +
         "<br>".html_safe +
-      (link_to 'Login', new_user_session_path)
+        (link_to 'Login', new_user_session_path)
+    end
+  end
+
+  def source_tracker_helper
+    if session[:source]
+      content_tag(:div, "Thanks for visiting me from #{session[:source]}", class: "source-greeting")
     end
   end
 
