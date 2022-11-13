c3d7f65659828aa14f26dbf700fb6f46f2524547
Adil Tuladhar <adiltuladhar77@gmail.com>
2022-11-14 00:33:18 +0545

    customized navbar width, application_footer_partial, login_logout in navbar, Learn More button functional

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
-  def login_register_button_toggle_helper
+  def login_register_button_toggle_helper(style)
     if current_user.is_a?(GuestUser)
-      (link_to 'Register', new_user_registration_path) +
-        "<br>".html_safe +
-        (link_to 'Login', new_user_session_path)
+      (link_to 'Register', new_user_registration_path, class: style) +
+        " ".html_safe +
+        (link_to 'Login', new_user_session_path, class: style)
     else
-      link_to 'Logout', destroy_user_session_path, method: :delete, data: { turbo_method: :delete }
+      link_to 'Logout', destroy_user_session_path, method: :delete, data: { turbo_method: :delete }, class: style
     end
   end
 
@@ -15,7 +15,7 @@ module ApplicationHelper
       content_tag(:div, "Thanks for visiting me from #{session[:source]}", class: "source-greeting")
     end
   end
-  
+
   def generate_copyright
     AadilViewTool::Renderer.cppyright 'Aadil RT', 'All rights reserved.'
   end
